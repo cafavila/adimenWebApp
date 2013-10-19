a2c50ad594441bc5239887b04688f231f6ab2d81
cafavila <cafavila@gmail.com>
2013-10-18 22:53:04 -0300

    Borrar directorios de sobra

--- js/adiscript.js
+++
@@ -1,9 +0,0 @@
-// Script de Funcionalidad de Prueba de tecnicas y trucos en Jquery, Javasript y otras tecnologias que mejoran la experiencia de usuario de nuestro sitio.
-$(document).ready(function ()
-{
-    $('.pull-me').click(function ()
-    {
-        $('.panel').slideToglee('slow');
-        console.log("Click");
-    });
-});
